de70608c2698284e7651f8fa6c41c85fd188f0b5
pushpendra <singh16195@gmail.com>
2024-10-05 17:07:15 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { createApi } from 'unsplash-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngDash';
+  unsplash = createApi({
+    accessKey: 'YO4lZLossOR5Kd-07E6mqC4BhotE6LHl5kw2fsG3XqY',
+  });
+  ngOnInit(): void {
+    const res = this.unsplash.photos.getRandom({ count: 1 });
+    res.then((x) => console.log(x.response));
+  }
 }
